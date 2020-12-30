40530f15df72775767e52cddf2fb1c6db1cfb087
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-29 23:34:48 -0800

    Build data model

--- src/App.js
+++ src/App.js
@@ -1,4 +1,40 @@
 function App() {
+  const appState = {
+    personalInfo: {
+      name: 'John Doe',
+      city: 'San Francisco'
+    },
+    schoolInfo: [
+      {
+        schoolName: 'USC',
+        location: 'Los Angeles',
+        courseOfStudy: 'English'
+      },
+      {
+        schoolName: 'The Odin Project',
+        location: 'Online',
+        courseOfStudy: 'Web development'
+      }
+    ],
+    workExperience: [
+      {
+        companyName: 'Bank',
+        location: 'Los Angeles',
+        title: 'Teller'
+      },
+      {
+        companyName: 'Website Builders',
+        location: 'Long Beach',
+        title: 'Intern'
+      },
+      {
+        companyName: 'Freelance',
+        location: 'Remote',
+        title: 'Software engineer'
+      }
+    ]
+  };
+
   return <div className='App'>Learn React </div>;
 }
 
